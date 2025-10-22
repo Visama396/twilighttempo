41c1c8a2bbc74f8872c91274d26692dd8ff06841
Victor <visama396@gmail.com>
2025-10-22 15:21:24 +0200

    update

---
+++ src/components/BuildsList.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react"
+
+function BuildsList() {
+    return (
+        <section className="p-2">
+            <header>
+                <h1 className="text-white text-3xl p-2">Sets terminados</h1>
+            </header>
+        </section>
+    )
+}
